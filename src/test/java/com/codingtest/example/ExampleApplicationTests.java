e66836a04259c608487df26b773f1353965f5bdc
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-09-13 16:46:09 +0900

    ver1

---
+++ src/test/java/com/codingtest/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingtest.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
