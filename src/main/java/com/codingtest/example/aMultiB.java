a08375e573c68dbbc02062230100645699ce35c6
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-09-15 14:43:39 +0900

    ver2

--- src/main/java/com/codingtest/example/aMultiB.java
+++ src/main/java/com/codingtest/example/aMultiB.java
@@ -1,13 +1,11 @@
 package com.codingtest.example;
 import java.util.Scanner;
 
-public class aMinusB {
+public class aMultiB {
     public static void main(String[] agrs){
         Scanner aa = new Scanner(System.in);
         int a,b;
-        a = aa.nextInt();
-        b = aa.nextInt();
-        System.out.println(a-b);
+        a =
     }
     
 }
