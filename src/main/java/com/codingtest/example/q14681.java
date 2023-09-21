158596c7b0270caea52745bb476d5fdd6551b8e5
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-09-21 10:52:56 +0900

    q14681

---
+++ src/main/java/com/codingtest/example/q14681.java
@@ -0,0 +1,14 @@
+package com.codingtest.example;
+
+import java.util.Scanner;
+
+public class q14681 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+
+	}
+
+}
