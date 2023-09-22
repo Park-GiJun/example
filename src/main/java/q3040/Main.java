646a0acd983658e5f38dcebdd75c2f2a4d1ab754
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-09-22 12:27:28 +0900

    .

---
+++ src/main/java/q3040/Main.java
@@ -0,0 +1,24 @@
+package q3040;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c, d, e, f, g, h, i;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		d = sc.nextInt();
+		e = sc.nextInt();
+		f = sc.nextInt();
+		g = sc.nextInt();
+		h = sc.nextInt();
+		i = sc.nextInt();
+		
+		
+		
+	}
+
+}
