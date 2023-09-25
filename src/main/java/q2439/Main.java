7f398ab9c995c29d1920d47dddaaa7e9d961965c
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-09-25 22:11:15 +0900

    9.25

--- src/main/java/q2439/Main.java
+++ src/main/java/q2439/Main.java
@@ -8,19 +8,19 @@ public class Main {
 //		첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
 //
 //		하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+
 		Scanner sc = new Scanner(System.in);
-		
-		String star = "*";
-		
-		int a = sc.nextInt();
+		int n = sc.nextInt();
 		sc.close();
-		
-		
-		for (int i =0;i<a;i++) {
-			System.out.printf("%i", " ");
-			System.out.print(star+"\n");
+
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= n - i; ++j) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; ++j) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
-
 	}
-
 }
