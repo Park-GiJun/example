3877fc916f95a869b7c4ac6d2f8ee06b6d1a1495
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-10-10 09:39:39 +0900

    2581

--- src/main/java/q1978/Main.java
+++ src/main/java/q1978/Main.java
@@ -28,7 +28,6 @@ public class Main {
 		br.close();
 	}
 
-	// 주어진 숫자가 소수인지 판별하는 함수
 	public static boolean isPrime(int num) {
 		if (num <= 1) {
 			return false;
@@ -40,8 +39,6 @@ public class Main {
 			return false;
 		}
 
-		// 5 이상의 소수는 6k ± 1 형태를 가짐 (k는 자연수)
-		// 따라서 6k ± 1 형태의 수만 검사하면 됨
 		for (int i = 5; i * i <= num; i += 6) {
 			if (num % i == 0 || num % (i + 2) == 0) {
 				return false;
